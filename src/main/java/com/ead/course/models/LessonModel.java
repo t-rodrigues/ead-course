bab4675b472fed6ca4af949c754bed730838d751
Thiago Rodrigues <thiagor_@live.com>
2021-12-12 20:29:32 -0300

    feat: add LessonModel

---
+++ src/main/java/com/ead/course/models/LessonModel.java
@@ -0,0 +1,37 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(of = "lessonId")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "tb_lessons")
+public class LessonModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID lessonId;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, length = 250)
+    private String description;
+
+    @Column(nullable = false)
+    private String videoUrl;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime createdAt;
+
+}
