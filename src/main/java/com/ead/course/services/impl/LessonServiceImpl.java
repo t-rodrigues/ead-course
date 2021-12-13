0b6ae8bb90c381ee65ebd0bf42e9782e92ba0f2b
Thiago Rodrigues <thiagor_@live.com>
2021-12-12 21:21:42 -0300

    feat: add LessonServiceImpl

---
+++ src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.services.LessonService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LessonServiceImpl implements LessonService {
+
+}
