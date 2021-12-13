6188f0b96befd34df84411f50ca63ec114b93783
Thiago Rodrigues <thiagor_@live.com>
2021-12-12 21:17:23 -0300

    feat: add LessonRepository

---
+++ src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -0,0 +1,12 @@
+package com.ead.course.repositories;
+
+import com.ead.course.models.LessonModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
+
+}
