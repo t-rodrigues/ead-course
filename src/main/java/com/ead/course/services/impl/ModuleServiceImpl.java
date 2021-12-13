0849a086bfd75fe051f6bf3540e682670ea5856c
Thiago Rodrigues <thiagor_@live.com>
2021-12-12 21:21:12 -0300

    feat: add ModuleServiceImpl

---
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.services.ModuleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModuleServiceImpl implements ModuleService {
+
+}
