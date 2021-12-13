f43e20b625f15ee69f5dd40919c2b3b1ff9c3992
Thiago Rodrigues <thiagor_@live.com>
2021-12-12 21:24:03 -0300

    feat: inject CourseRepository

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,9 +1,16 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.repositories.CourseRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public CourseServiceImpl(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
 
 }
