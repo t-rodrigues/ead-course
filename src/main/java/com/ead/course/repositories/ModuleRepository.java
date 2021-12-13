1b50d4ac88f9e1513db8d608793841d94ffa5c4e
Thiago Rodrigues <thiagor_@live.com>
2021-12-12 21:16:40 -0300

    feat: add ModuleRepository

---
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -0,0 +1,12 @@
+package com.ead.course.repositories;
+
+import com.ead.course.models.ModuleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+}
